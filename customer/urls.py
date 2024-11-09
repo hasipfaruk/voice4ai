5f400badb4078aa2d533a5ae19cbd0ac592693ce
Zeeshan Afzal <zeeshan.afzal.37073@gmail.com>
2024-11-09 19:00:41 +0500

    abdullah

---
+++ customer/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    # path('', views.signup, name='index'),
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('index/', views.login, name='home'),
+]
+
---
+++ staticfiles/customer/js/customer/validation.js
@@ -0,0 +1,80 @@
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const phone2numeric_input = document.getElementById('phone2numeric-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
+const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+  let errors = []
+
+  if(firstname_input){
+    // If we have a firstname input then we are in the signup
+    errors = getSignupFormErrors(firstname_input.value, phone2numeric_input.value, password_input.value, repeat_password_input.value)
+  }
+  else{
+    // If we don't have a firstname input then we are in the login
+    errors = getLoginFormErrors(phone2numeric_input.value, password_input.value)
+  }
+
+  if(errors.length > 0){
+    // If there are any errors
+    e.preventDefault()
+    error_message.innerText  = errors.join(". ")
+  }
+})
+
+function getSignupFormErrors(firstname, Phone, password, repeatPassword){
+  let errors = []
+
+  if(firstname === '' || firstname == null){
+    errors.push('Firstname is required')
+    firstname_input.parentElement.classList.add('incorrect')
+  }
+  if(Phonehone === '' || Phone == null){
+    errors.push('Email is required')
+    phone2numeric_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password.length < 8){
+    errors.push('Password must have at least 8 characters')
+    password_input.parentElement.classList.add('incorrect')
+  }
+  if(password !== repeatPassword){
+    errors.push('Password does not match repeated password')
+    password_input.parentElement.classList.add('incorrect')
+    repeat_password_input.parentElement.classList.add('incorrect')
+  }
+
+
+  return errors;
+}
+
+function getLoginFormErrors(Phone, password){
+  let errors = []
+
+  if(Phone === '' || Phone == null){
+    errors.push('Phone is required')
+    phone2numeric_input.parentElement.classList.add('incorrect')
+  }
+  if(password === '' || password == null){
+    errors.push('Password is required')
+    password_input.parentElement.classList.add('incorrect')
+  }
+
+  return errors;
+}
+
+const allInputs = [firstname_input, phone2numeric_input, password_input, repeat_password_input].filter(input => input != null)
+
+allInputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect')
+      error_message.innerText = ''
+    }
+  })
+})
